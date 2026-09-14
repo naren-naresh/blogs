e9bada457ad8109f933c35f96336711d98f211aa
Naresh <narendirannaresh396@gmail.com>
2026-09-14 17:37:39 +0530

    initial commit - blogs microservice inital

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
-import PostCreate from './postCreate'
-
+import PostCreate from "./postCreate";
 
 export default () => {
-  return  (
-    <div className='container'>
+  return (
+    <div className="container">
       <h1>Create Post</h1>
-      <PostCreate/>
+      <PostCreate />
     </div>
-  )};
+  );
+};
