849dcca8054c7237c99fecd8af1d93810210f2e5
Naresh <narendirannaresh396@gmail.com>
2026-09-14 19:35:34 +0530

    client updated create comment page added

--- client/src/commentCreate.jsx
+++ client/src/commentCreate.jsx
@@ -1,31 +1,29 @@
-import { useState } from "react"
+import { useState } from "react";
 import axios from "axios";
 
+export default function CommentCreate({ postId }) {
+  const [content, setContent] = useState("");
 
-export default function CommentCreate({postId}) {
+  const onsubmit = async (event) => {
+    event.preventDefault();
+    await axios.post(`http://localhost:4001/posts/${postId}/comments`, {
+      content,
+    });
 
-    const [content, setContent] = useState('')
-
-    const onsubmit = async(event) => {
-        event.preventDefault();
-        await axios.post(`http://localhost:4001/posts/${postId}/comments`,{
-            content
-        });
-
-        setContent('');
-    }
+    setContent("");
+  };
   return (
     <div>
       <form onSubmit={onsubmit} className="form-group">
         <label>New Comment</label>
-        <input 
-          value={content} 
-          onChange={e => setContent(e.target.value)}
-          type="text" 
-          className="form-control" 
+        <input
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+          type="text"
+          className="form-control"
         />
-       <button className="btn btn-primary mt-2">Submit</button>
+        <button className="btn btn-primary mt-2">Submit</button>
       </form>
     </div>
-  )
+  );
 }
